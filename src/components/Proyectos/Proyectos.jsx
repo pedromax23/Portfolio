465d96f21e4b35c60d31926c73a9a5ff785c1948
pedromax23 <pedromorgade1234@gmail.com>
2024-04-23 14:44:03 -0300

    Agregando express para la Api

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -2,11 +2,27 @@ import React from 'react'
 import './Proyectos.css'
 
 import GestiosGastosImg from '../../assets/GestiosGastos.png'
+import Cumpleaños from '../../assets/Cumpleaños.png'
 
 function Proyectos() {
   return (
-    <section className='sect-experiencia'>
+    <section className='sect-proyectos'>
       <h2><i className="fa-solid fa-code"></i>Proyectos</h2>
+      <article className='article-proyect'>
+        <img src={Cumpleaños} alt="" />
+        <div>
+          <h3>Galeriad de fotos</h3>
+          <div className='tecnologias'>
+            <span className='tec-react'><i className="fa-brands fa-react"></i> React.js</span>
+            <span className='tec-express'><i className="fa-brands fa-react"></i> Express.js</span>
+          </div>
+          <p>Este proyecto lo he creado para exhibir las hermosas fotos que tomamos durante mi cumpleaños.
+            <br />
+            He utilizado Express.js para crear la API REST y recuperar las fotos. Además, he empleado React para la interfaz visual y poder visualizar las imágenes.
+          </p>
+          <a href="https://pedromax23.github.io/Fotos-Pedro-React/"><i className="fa-solid fa-link"></i> Preview</a>
+        </div>
+      </article>
       <article className='article-proyect'>
         <img src={GestiosGastosImg} alt="" />
         <div>
@@ -14,7 +30,7 @@ function Proyectos() {
           <div className='tecnologias'>
             <span><i className="fa-brands fa-react"></i> React.js</span>
           </div>
-          <p>"Este es un proyecto que realicé siguiendo un vídeo de Fazt Code. Esta aplicación te permite registrar los gastos y mostrarlos en un gráfico.</p>
+          <p>Este es un proyecto que realicé siguiendo un vídeo de Fazt Code. Esta aplicación te permite registrar los gastos y mostrarlos en un gráfico.</p>
           <a href="https://pedromax23.github.io/github-pages/"><i className="fa-solid fa-link"></i> Preview</a>
         </div>
       </article>
